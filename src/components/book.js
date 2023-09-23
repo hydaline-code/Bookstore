a3181538582c39773a0986ec6a94bec92980a664
hydaline-code <djougancharlen@.com>
2023-09-23 16:31:53 +0200

    Add some stylings

--- src/components/book.js
+++ src/components/book.js
@@ -1,16 +1,39 @@
 
 import React from 'react';
 
-function Book({ title, author, onDelete }) {
+function BookCard({ title, author, onDelete }) {
+  const progress = 50; 
+  const currentChapter = 'Chapter 3'; 
+  const lesson = 'Lesson 7'; 
+
   return (
-    <div className="bookcard">
-   
-      <h3>{title}</h3>
-      <p>{author}</p>
-   
-    <button onClick={onDelete}>Delete</button>
-  </div>
+    <div className="book-card">
+
+      <div className="book-details">
+        <h2 className="book-title">{title}</h2>
+        <p className="book-author">{author}</p>
+        <button onClick={onDelete}>Delete</button>
+      </div>
+
+      <div className="progress-bar">
+  <div className="Oval-2"></div>
+  <span className="-Percent-Complete Text-Style-10">
+    64%
+    <p className='state'>completed</p>
+  </span>
+
+</div>
+
+
+      <div className="chapter-details">
+        <span className="chapter-label">Current Chapter:</span>
+        <span className="chapter-info">{currentChapter}</span>
+        <span className="lesson-label">Current Lesson:</span>
+        <span className="lesson-info">{lesson}</span>
+      </div>
+
+    </div>
   );
 }
 
-export default Book;
+export default BookCard;
