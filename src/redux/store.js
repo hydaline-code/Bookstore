2a7431dc57083fb6a0ed0d0cf84eb620b0330567
hydaline-code <djougancharlen@.com>
2023-09-27 15:35:42 +0200

    Add redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import bookReducer from "./books/booksSlice";
+import { categoriesReducer } from "./categories/categoriesSlice";
 
-const storeConfig = ({
-
-  reducer:
+const store = storeConfig ({
+  reducer: {
+    books: bookReducer, // Include the books slice reducer
+    categories: categoriesReducer, // Include the categories slice reducer
+   
+  },
 })
+
+export default store;
+
