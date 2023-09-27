cd132e4a2cb1c1a00a87c2facc27f8e67cb940bd
hydaline-code <djougancharlen@.com>
2023-09-27 15:30:07 +0200

    Add redux to the project and configured reducers

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+  //initializing an empty array of book
+  categories: [],
+}
+const  categoriesSlice = createSlice({
+ name: 'categories',
+ initialState,
+ reducers: {
+    checkstatus: (state) => {
+    return  "Under construction"
+   }
+   //checking the categories
+ },
+
+})
+
+export const { reducer: categoriesReducer, actions: categoriesActions } = categoriesSlice;
+
+export default categoriesSlice.reducer;
