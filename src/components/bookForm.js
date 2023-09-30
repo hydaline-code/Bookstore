78040f6ae47430d7e0231f8a29d285eb46d78173
hydaline-code <djougancharlen@.com>
2023-09-30 08:58:58 +0200

    Update bookcard with actions

--- src/components/bookForm.js
+++ src/components/bookForm.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice'; // Import your addBook action
+import { addBook } from '../redux/books/booksSlice'; 
 import { v4 as uuidv4 } from 'uuid';
 
 function BookForm({ books}) {
