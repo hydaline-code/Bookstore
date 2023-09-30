d1d70996eb649d3d085561cb8bda89c8ac2a92f8
hydaline-code <djougancharlen@.com>
2023-09-30 08:46:01 +0200

    Configure store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-// src/redux/store.js
+
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducerDefault from './categories/categoriesSlice';
